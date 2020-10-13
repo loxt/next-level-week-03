73d22e8be0b6540cda1efd97041ffd0534eda2ec
loxt <emannuelloxt@hotmail.com>
2020-10-13 15:38:51 -0400

    fix: error handler of body

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+interface ValidationErrors {
+  [key: string]: string[];
+}
+
+const errorHandler: ErrorRequestHandler = (err, req, res) => {
+  if (err instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+
+    err.inner.forEach(e => {
+      errors[e.path] = e.errors;
+    });
+
+    return res.status(400).json({ msg: 'Validation fails', errors });
+  }
+
   console.error(err);
 
   return res.status(500).json({ msg: 'Internal server error' });
