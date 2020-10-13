91d9439ba03bbb501630c6d1be74a2fed7877c93
loxt <emannuelloxt@hotmail.com>
2020-10-12 22:20:41 -0400

    feat: add react-router-dom integration

---
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrphanagesMap() {
+  return (
+    <h1>
+      Orfanatos
+    </h1>
+  );
+}
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import OrphanagesMap from "./pages/OrphanagesMap";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Route exact path='/' component={Landing} />
+      <Route path='/app' component={OrphanagesMap} />
+    </BrowserRouter>
+  )
+}
