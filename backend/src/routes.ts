a31a56295a691f9861856365807dd9eba5c388e7
loxt <emannuelloxt@hotmail.com>
2020-10-13 13:18:25 -0400

    feat: add show orphanage details route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from './controllers/Orphanages.controller';
 const routes = Router();
 
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
