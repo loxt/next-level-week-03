c8f19c268ca1fcb728a6cb220c2fec1af8d2cb9a
loxt <emannuelloxt@hotmail.com>
2020-10-15 16:19:11 -0400

    chore: show marker in map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View, Dimensions} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.2901377,
+          longitude: -53.8128811,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}>
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -22.2901377,
+            longitude: -53.8128811,
+          }}/>
+      </MapView>
     </View>
   );
 }
@@ -14,8 +30,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
